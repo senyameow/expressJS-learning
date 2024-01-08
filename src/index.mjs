9db159a5d6b744683b6d8f764e546ea0b711212a
senyameow <arseniyxd@gmail.com>
2024-01-08 22:54:14 +0700

    query params

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,8 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+const users = [{ id: 1, name: 'senya', age: 18 }, { id: 20 }]
+
 app.listen(PORT, () => {
     console.log(`port is ${PORT}`)
 })
@@ -13,9 +15,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/users', (req, res) => {
-    res.send([{ id: 1, name: 'senya', age: 18 }, { id: 20 }])
+    const { query: { filter, value } } = req
+    console.log(filter, value)
+    if (filter && value) return res.send(users.filter(u => u?.[filter]?.includes(value)))
+    return res.send(users)
 })
 
 app.get('/api/users/:id', (req, res) => {
-    res.send(req.params)
+    const parsedId = parseInt(req.params.id)
+    if (isNaN(parsedId)) return res.status(400).send({ message: 'bad request' })
+
+    const user = users.find(user => user.id === parsedId)
+    if (!user) return res.status(404)
+    return res.send(user)
 })
